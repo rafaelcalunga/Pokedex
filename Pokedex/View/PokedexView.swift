a0c4f6a5b51bfc8d18ba7a7df75009870c0a61ec
Rafael Calunga <rafaelcalunga@gmail.com>
2021-05-29 17:31:52 -0400

    Pokemon cell UI View

---
+++ Pokedex/View/PokedexView.swift
@@ -0,0 +1,20 @@
+//
+//  PokedexView.swift
+//  Pokedex
+//
+//  Created by Rafael Calunga on 2021-05-29.
+//
+
+import SwiftUI
+
+struct PokedexView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct PokedexView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokedexView()
+    }
+}
