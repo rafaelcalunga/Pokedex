28186c461db7a85b83c0dbc6af444172eb821f87
Rafael Calunga <rafaelcalunga@gmail.com>
2021-05-29 17:04:55 -0400

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Rafael Calunga on 2021-05-29.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
